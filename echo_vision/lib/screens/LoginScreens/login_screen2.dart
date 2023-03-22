9d4e7a323c087c0b80f4daa63942a5da2b6e2984
Akashiutchiha <christianhonore2003@gmail.com>
2023-03-22 19:53:47 +0100

    fix: Updated Login Screens

---
+++ echo_vision/lib/screens/LoginScreens/login_screen2.dart
@@ -0,0 +1,78 @@
+import 'package:echo_vision/components/BaseScreen.dart';
+import 'package:echo_vision/screens/home.dart';
+import 'package:flutter/material.dart';
+
+import '../components/constants.dart';
+import '../components/my_text_field.dart';
+import '../components/text_button_style.dart';
+
+class Login2 extends StatelessWidget {
+  const Login2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          margin: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                backgroundColor: kPrimaryColor,
+                radius: 30.0,
+                child: IconButton(
+                  color: Colors.black,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    size: 30.0,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 40.0,
+              ),
+              Text(
+                'Tell us your Password',
+                style: kConstantStyle,
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              MyTextField(
+                title: "Password",
+              ),
+              Expanded(
+                child: Center(
+                  child: Container(
+                    child: CircleAvatar(
+                      backgroundColor: kPrimaryColor,
+                      radius: 70.0,
+                      child: Image(
+                        image: AssetImage("images/Frame 7.png"),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 50,
+                width: double.infinity,
+                child: TextButtonStyle(
+                  text: "Confirm",
+                  color: kPrimaryColor,
+                  child: Home(),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
