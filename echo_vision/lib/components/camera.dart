91010e128f15a6da0f4d4152edf951e64ae00360
Akashiutchiha <christianhonore2003@gmail.com>
2023-04-12 23:03:05 +0100

    feat: Working on the vision functionality

--- echo_vision/lib/components/camera.dart
+++ echo_vision/lib/components/camera.dart
@@ -1,55 +1,121 @@
-import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
-import 'package:echo_vision/main.dart';
+// import 'package:flutter/material.dart';
+// import 'package:camera/camera.dart';
+// import 'package:tflite/tflite.dart';
+// import 'package:echo_vision/main.dart';
 
-/// CameraApp is the Main Application.
-class CameraApp extends StatefulWidget {
-  /// Default Constructor
-  const CameraApp({Key? key}) : super(key: key);
+// /// CameraApp is the Main Application.
+// class CameraApp extends StatefulWidget {
+//   /// Default Constructor
+//   const CameraApp({Key? key}) : super(key: key);
 
-  @override
-  State<CameraApp> createState() => _CameraAppState();
-}
+//   @override
+//   State<CameraApp> createState() => _CameraAppState();
+// }
 
-class _CameraAppState extends State<CameraApp> {
-  late CameraController controller;
+// class _CameraAppState extends State<CameraApp> {
+//   CameraImage? cameraImage;
+//   late CameraController controller;
+//   List<dynamic>? results;
+//   bool isDetecting = false;
 
-  @override
-  void initState() {
-    super.initState();
-    controller = CameraController(cameras[0], ResolutionPreset.max);
-    controller.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-    }).catchError((Object e) {
-      if (e is CameraException) {
-        switch (e.code) {
-          case 'CameraAccessDenied':
-            // Handle access errors here.
-            break;
-          default:
-            // Handle other errors here.
-            break;
-        }
-      }
-    });
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     loadModel().then((value) {
+//       setState(() {
+//         results;
+//       });
+//     });
+//     controller = CameraController(cameras[0], ResolutionPreset.max);
+//     controller.initialize().then((_) {
+//       if (!mounted) {
+//         return;
+//       }
+//       setState(() {
+//         controller.startImageStream((image) {
+//           cameraImage = image;
+//           if (!isDetecting) {
+//             isDetecting = true;
+//             Tflite.runModelOnFrame(
+//               bytesList: image.planes.map((plane) {
+//                 return plane.bytes;
+//               }).toList(),
+//               imageHeight: image.height,
+//               imageWidth: image.width,
+//               imageMean: 127.5,
+//               imageStd: 127.5,
+//               rotation: 90,
+//               numResults: 2,
+//               threshold: 0.1,
+//               asynch: true,
+//             ).then((recognitions) {
+//               setState(() {
+//                 results = recognitions;
+//               });
+//               isDetecting = false;
+//             });
+//           }
+//         });
+//       });
+//     }).catchError((Object e) {
+//       if (e is CameraException) {
+//         switch (e.code) {
+//           case 'CameraAccessDenied':
+//             // Handle access errors here.
+//             break;
+//           default:
+//             // Handle other errors here.
+//             break;
+//         }
+//       }
+//     });
+//   }
 
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
+//   Future loadModel() async {
+//     await Tflite.loadModel(
+//       model: 'assets/model/model.tflite',
+//       labels: 'assets/model/label.txt',
+//       numThreads: 1,
+//       isAsset: true,
+//     );
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    if (!controller.value.isInitialized) {
-      return Container();
-    }
-    return MaterialApp(
-      home: CameraPreview(controller),
-    );
-  }
-}
+//   @override
+//   void dispose() {
+//     controller.dispose();
+//     Tflite.close();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     if (!controller.value.isInitialized) {
+//       return Container();
+//     }
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Stack(
+//           children: [
+//             Expanded(child: CameraPreview(controller)),
+//             Center(
+//               child: Positioned(
+//                 bottom: 150,
+//                 child: Container(
+//                   padding: const EdgeInsets.all(10),
+//                   color: Color.fromARGB(137, 255, 255, 255),
+//                   child: Text(
+//                     results != null ? results![0]['label'] : 'analyzing...',
+//                     style: const TextStyle(
+//                       color: Colors.white,
+//                       fontSize: 30.0,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
---
+++ echo_vision/lib/components/camera2.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:tflite/tflite.dart';
+import 'package:echo_vision/main.dart';
+
+/// CameraApptwo is the Main Application.
+class CameraApptwo extends StatefulWidget {
+  /// Default Constructor
+  CameraApptwo({Key? key}) : super(key: key);
+
+  @override
+  State<CameraApptwo> createState() => _CameraApptwoState();
+}
+
+class _CameraApptwoState extends State<CameraApptwo> {
+  CameraImage? cameraImage;
+
+  late CameraController controller;
+
+  List<dynamic>? results;
+
+  String output = 'he';
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel();
+    loadCamera();
+  }
+
+  loadCamera() async {
+    controller = CameraController(cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      } else {
+        setState(() {
+          controller.startImageStream((image) {
+            cameraImage = image;
+            runModel();
+          });
+        });
+      }
+    });
+  }
+
+  runModel() async {
+    if (cameraImage != null) {
+      var predictions = await Tflite.runModelOnFrame(
+        bytesList: cameraImage!.planes.map((plane) {
+          return plane.bytes;
+        }).toList(),
+        imageHeight: cameraImage!.height,
+        imageWidth: cameraImage!.width,
+        imageMean: 127.5,
+        imageStd: 127.5,
+        rotation: 90,
+        numResults: 2,
+        threshold: 0.1,
+        asynch: true,
+      );
+      for (var element in predictions!) {
+        setState(() {
+          output = element['label'];
+        });
+      }
+    }
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+      model: 'assets/model/model.tflite',
+      labels: 'assets/model/label.txt',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            Expanded(child: CameraPreview(controller)),
+            Center(
+              child: Positioned(
+                bottom: 150,
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  color: Color.fromARGB(137, 255, 255, 255),
+                  child: Text(
+                    output,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
